79d326ee8cdd3d80abd792ccc11f23cec3586bb6
Tihan Fernando <tihanlakmitha@gmail.com>
2023-02-15 11:12:26 +0530

    new fetch address

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('https://codex-lawa.onrender.com', {
+  const response = await fetch('https://codex-2fze.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
