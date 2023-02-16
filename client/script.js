9f623f2c86011b42053a120fc129984cd3bac965
Tihan Fernando <tihanlakmitha@gmail.com>
2023-02-16 12:50:36 +0530

    add the authorization header

--- client/script.js
+++ client/script.js
@@ -90,6 +90,8 @@ const handleSubmit = async (e) => {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+            
         },
         body: JSON.stringify({
             prompt: data.get('prompt')
